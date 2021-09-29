0064ef2414512a9dda749f51946bcd06f51740dc
Dongu <ehdrn1205@gmail.com>
2021-09-29 14:40:46 +0000

    헤더 및 사이드바 구현(0929)

--- front/client/src/App.vue
+++ front/client/src/App.vue
@@ -2,17 +2,15 @@
 	<div>		
 		<!-- <Home /> -->
 		<Header />
-		<SideBar />
 	</div>
 </template>
 
 <script>
 	// import Home from './views/Home'
 	import Header from './components/Header'
-	import SideBar from './components/SideBar'
 
 	export default {
-		components: {Header, SideBar, /*Home*/},
+		components: {Header, /*Home*/},
 		// computed: {
 		// 	user() {
 		// 		return this.$store.state.user;
--- front/client/src/components/Header.vue
+++ front/client/src/components/Header.vue
@@ -1,18 +1,63 @@
 <template>
-	<nav class="navbar navbar-light bg-light">
-  <div class="container">
-    <a class="navbar-brand" href="#">
-      <img src="../assets/logo.png" alt="" width="200" height="100">
-    </a>
-  </div>
-	</nav>
+	<section id="main-container">
+		<header id="header">
+			<section class="inner">
+			<a href="#"><img src="../assets/logo2.png" alt="" width="150"></a>
+			<div id="right-icon">
+				<a href="#"><img src="../assets/home1.png" alt="home" width="27"></a>
+				<a href="#"><img src="../assets/follower1.png" alt="home" width="27"></a>
+				<a href="#"><img src="../assets/bookmark1.png" alt="home" width="27"></a>
+				<a id="profileIcon" href="#">H</a>
+				<a href="#"><img src="../assets/edit1.png" alt="home" width="27"></a>
+	</div>
+	</section>
+	</header>
+	</section>
 </template>
 
 <script>
 	export default {
-		name: 'header'
+		name: 'header',
+		data() {
+			return {
+				username: 'H'
+			};
+		}
 	}
-</script>
+</script>	
 
 <style>
+	#header {
+		position: absolute;
+		width: 100%;
+		left: 0;
+		top: 0;
+		z-index: 999;
+		border-bottom: 1px solid;
+	}
+	#header .inner {
+		width: 675px;
+		height: 100px;
+		margin: 0 auto;
+		display: flex;
+		justify-content: space-between;
+		align-items: center;
+	}
+	#header #right-icon {
+		width: 190px;
+		display: flex;
+		justify-content: space-between;
+		align-items: center;
+	}
+	#header #right-icon #profileIcon{
+		display: inline-block;
+		border: 1.5px solid;
+		border-radius: 50%;
+		color: black;
+		text-decoration: none;
+		width: 33px;
+		height: 33px;
+		text-align: center;
+		font-size: 19px;
+	}
 </style>
